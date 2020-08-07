6c2c2e9391c656244899123e520b5a7ff878565c
申致尧 <1764203060@qq.com>
2020-08-07 22:46:56 +0800

    feedback

--- plugins/getUser.py
+++ plugins/getUser.py
@@ -1,14 +1,14 @@
 import json
 
 def getUsers():
-    f = open('../users.json', 'r')
+    f = open('users.json', 'r')
     users = f.read()
     data = json.loads(users)
     f.close()
     return data
 
 def saveUsers(list):
-    with open('../users.json', "r+") as f:
+    with open('users.json', "r+") as f:
         read_data = f.read()
         f.seek(0)
         f.truncate()
